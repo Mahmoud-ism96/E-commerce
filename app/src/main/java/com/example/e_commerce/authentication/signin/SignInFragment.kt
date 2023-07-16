4cec7fda9d225e5db444b66123d661b0cd75604f
Mahmoud Ismail <mahmoud.ism96@gmail.com>
2023-07-17 02:36:57 +0300

    Add start up fragment Add sign in fragment Add sign up fragment

---
+++ app/src/main/java/com/example/e_commerce/authentication/signin/SignInFragment.kt
@@ -0,0 +1,25 @@
+package com.example.e_commerce.authentication.signin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.e_commerce.R
+import com.example.e_commerce.databinding.FragmentSignInBinding
+import com.example.e_commerce.databinding.FragmentStartupBinding
+
+class SignInFragment : Fragment() {
+
+    private lateinit var binding : FragmentSignInBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSignInBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+}
