90e9927af117423e2012b4603409c8ef39f72cda
moali2605 <107775491+moali2605@users.noreply.github.com>
2023-07-17 02:20:26 +0300

    bottom navigation bar

--- app/src/main/java/com/example/e_commerce/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.e_commerce
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.e_commerce.databinding.ActivityMainBinding
 import com.example.e_commerce.intro.MyIntro
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val intent = Intent(this, MyIntro::class.java)
         startActivity(intent)
+        binding.hallo.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
