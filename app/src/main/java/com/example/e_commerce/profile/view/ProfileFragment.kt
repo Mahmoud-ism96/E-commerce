d2d45027fa135755ac2933fd3ec7a0fec3bd279a
Hassan kamal <118057412+hassankamal87@users.noreply.github.com>
2023-07-17 22:16:21 +0300

    create ProfileFragment UI create AccountDetailsFragment UI create MapFragment UI

--- app/src/main/java/com/example/e_commerce/profile/view/ProfileFragment.kt
+++ app/src/main/java/com/example/e_commerce/profile/view/ProfileFragment.kt
@@ -5,16 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.e_commerce.R
+import com.example.e_commerce.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvAccountDetails.setOnClickListener {
+            val navController = Navigation.findNavController(view)
+            navController.navigate(R.id.action_profileFragment2_to_accountDetailsFragment)
+        }
+
+        binding.btnOpenLanguage.setOnClickListener {
+            if(binding.cvLanguage.visibility == View.VISIBLE){
+                binding.cvLanguage.visibility = View.GONE
+            }else{
+                binding.cvLanguage.visibility = View.VISIBLE
+            }
+        }
+
+        binding.tvLogOut.setOnClickListener {
+            TODO("Navigate To Register screen")
+        }
+
+        binding.tvOrderDetails.setOnClickListener {
+            TODO("Navigate To orders screen")
+        }
+    }
 }
