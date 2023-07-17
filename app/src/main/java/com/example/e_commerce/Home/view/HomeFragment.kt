3291a479fb871cfb23f51c5aaee1601c09fced33
moali2605 <107775491+moali2605@users.noreply.github.com>
2023-07-17 22:51:46 +0300

    Finish HomeFragment UI Finish ListOfProductFragment UI Finish OrdersFragment UI

--- app/src/main/java/com/example/e_commerce/Home/view/HomeFragment.kt
+++ app/src/main/java/com/example/e_commerce/Home/view/HomeFragment.kt
@@ -6,15 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.e_commerce.R
+import com.example.e_commerce.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
+    private lateinit var binding:FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding= FragmentHomeBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 }
