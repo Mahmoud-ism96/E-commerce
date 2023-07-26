1be87fd79d53fd4c41a18d8b520668189ba6fe36
Mahmoud Ismail <mahmoud.ism96@gmail.com>
2023-07-26 14:28:32 +0300

    Finalize wishlist UI

--- app/src/main/java/com/example/e_commerce/wishlist/view/WishListFragment.kt
+++ app/src/main/java/com/example/e_commerce/wishlist/view/WishListFragment.kt
@@ -1,23 +1,44 @@
 package com.example.e_commerce.wishlist.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.e_commerce.databinding.FragmentWishListBinding
+import com.example.e_commerce.model.pojo.draftorder.response.DraftResponse
+import com.example.e_commerce.model.pojo.draftorder.response.LineItem
+import com.example.e_commerce.model.pojo.draftorder.send.SendDraftOrder
+import com.example.e_commerce.model.pojo.draftorder.send.SendDraftRequest
+import com.example.e_commerce.model.pojo.draftorder.send.SendLineItem
 import com.example.e_commerce.model.repo.Repo
 import com.example.e_commerce.services.db.ConcreteLocalSource
+import com.example.e_commerce.services.network.ApiState
 import com.example.e_commerce.services.network.ConcreteRemoteSource
+import com.example.e_commerce.utility.Constants.WISHLIST_KEY
 import com.example.e_commerce.wishlist.viewmodel.WishListViewModel
 import com.example.e_commerce.wishlist.viewmodel.WishListViewModelFactory
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class WishListFragment : Fragment() {
 
     private lateinit var binding: FragmentWishListBinding
     private lateinit var _viewModelFactory: WishListViewModelFactory
     private lateinit var _viewModel: WishListViewModel
+
+    private lateinit var wishlistID: String
+    private lateinit var lastLineItems: List<LineItem>
+
+    private lateinit var wishlistAdapter: WishListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -33,55 +54,112 @@ class WishListFragment : Fragment() {
             requireActivity(), _viewModelFactory
         )[WishListViewModel::class.java]
 
-//        wishlistRecycleAdapter = CartAdapter(onPlusClick = { _, quantity ->
-//            val newQuantity = quantity + 1
-//            // cartViewModel.updateQuantityForItem(id, newQuantity)
-//        }, onMinusClick = { _, quantity ->
-//            if (quantity > 1) {
-//                val newQuantity = quantity - 1
-//                //   cartViewModel.updateQuantityForItem(id, newQuantity)
-//            } else {
-//                // cartViewModel.deleteItemFromCart(id)
-//            }
-//        }, onItemClick = {
-//            val action = CartFragmentDirections.actionCartFragment2ToProductDetailsFragment(it)
-//            findNavController().navigate(action)
-//        })
-//
-//        binding.rvWishlist.apply {
-//            adapter = wishlistRecycleAdapter
-//            layoutManager = LinearLayoutManager(requireContext()).apply {
-//                orientation = RecyclerView.VERTICAL
-//            }
-//        }
-//
-//        if (FirebaseAuth.getInstance().currentUser != null) {
-//
-//            val wishlistID = _viewModel.readStringFromSettingSP(WISHLIST_KEY)
-//
-//            _viewModel.getDraftOrderByDraftId(wishlistID.toLong())
-//
-//            lifecycleScope.launch {
-//                _viewModel.wishlistDraftOrderFlow.collectLatest {
-//                    when (it) {
-//                        is ApiState.Loading -> {
-//                            Log.w(TAG, "loading:")
-//                        }
-//
-//                        is ApiState.Success -> {
-//                            val draftResponse: DraftResponse = it.data as DraftResponse
-//                            wishlistRecycleAdapter.submitList(draftResponse.draft_order.line_items)
-//                        }
-//
-//                        is ApiState.Failure -> {
-//                            Log.w(TAG, "error:")
-//                        }
-//                    }
-//                }
-//            }
-//        }
+        wishlistAdapter = WishListAdapter()
+        deleteWhenSwipe()
+
+        binding.rvWishlist.apply {
+            adapter = wishlistAdapter
+            layoutManager = LinearLayoutManager(requireContext()).apply {
+                orientation = RecyclerView.VERTICAL
+            }
+        }
+
+        if (FirebaseAuth.getInstance().currentUser != null) {
+
+            wishlistID = _viewModel.readStringFromSettingSP(WISHLIST_KEY)
+
+            _viewModel.getDraftOrderByDraftId(wishlistID.toLong())
+
+            lifecycleScope.launch {
+                _viewModel.wishlistDraftOrderFlow.collectLatest {
+                    when (it) {
+                        is ApiState.Loading -> {
+                            Log.w("TAG", "loading:")
+                        }
+
+                        is ApiState.Success -> {
+                            val draftResponse: DraftResponse = it.data as DraftResponse
+                            createListForAdapter(draftResponse)
+                        }
+
+                        is ApiState.Failure -> {
+                            Log.w("TAG", "error:")
+                        }
+                    }
+                }
+            }
+        }
 
         return binding.root
+    }
+
+    private fun createListForAdapter(draftResponse: DraftResponse) {
+        lastLineItems = draftResponse.draft_order.line_items
+        val viewedLineItems = lastLineItems.filterIndexed { index, _ ->
+            index > 0
+        }
+        wishlistAdapter.submitList(viewedLineItems)
+    }
+
+    private fun deleteWhenSwipe() {
+        val itemTouchHelperCallBack = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val lineItem = wishlistAdapter.currentList[position]
+//                binding.relodGroub.visibility = View.VISIBLE
+                deleteItemFromCart(lineItem)
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallBack)
+        itemTouchHelper.attachToRecyclerView(binding.rvWishlist)
+    }
+
+    private fun deleteItemFromCart(lineItem: LineItem) {
+        Toast.makeText(
+            requireContext(),
+            "Product removed from wishlist.",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        val updatedItems = lastLineItems.filter { it.id != lineItem.id }
+        lastLineItems = updatedItems
+
+        if (lastLineItems.isNotEmpty()) {
+            val viewedLineItems = lastLineItems.filterIndexed { index, _ ->
+                index > 0
+            }
+            wishlistAdapter.submitList(viewedLineItems)
+        }
+
+        val newSendLineItems = mutableListOf<SendLineItem>()
+        for (item in lastLineItems) {
+            newSendLineItems.add(
+                SendLineItem(item.variant_id, item.quantity, item.properties)
+            )
+        }
+        lifecycleScope.launch {
+            _viewModel.removeLineItem(
+                wishlistID.toLong(), SendDraftRequest(
+                    SendDraftOrder(
+                        newSendLineItems,
+                        FirebaseAuth.getInstance().currentUser!!.email!!,
+                        WISHLIST_KEY
+                    )
+                )
+            )
+        }
     }
 
 }
